0bd19278825017b6a1f15dce1c8a8f85d247e9db
mjachowdhury <study.computing2016@gmail.com>
2017-08-23 01:37:17 +0100

    Exception handle
    -Exception handle was created
    -error.jsp was created

--- spring-boot-first-web-application/src/main/java/com/alomsoftware/springboot/web/controller/TodoController.java
+++ spring-boot-first-web-application/src/main/java/com/alomsoftware/springboot/web/controller/TodoController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
 import com.alomsoftware.springboot.web.model.Todo;
 import com.alomsoftware.springboot.web.service.TodoService;
 
--- spring-boot-first-web-application/src/main/java/com/alomsoftware/springboot/web/controller/WelcomeController.java
+++ spring-boot-first-web-application/src/main/java/com/alomsoftware/springboot/web/controller/WelcomeController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 //i can use sessionAttributes or spring security eith way i can do 
 @Controller 
